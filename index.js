365d34aee16dc0c2fb792a0c0f2718d168370d8e
Daniel Muhia <muhiadan17@gmail.com>
2025-05-28 13:54:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove the main element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new h1 element and assign it to newHeader
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Daniel is the champion'; 
+
+// Append the new h1 element to the body
+document.body.appendChild(newHeader);
